bb74e8a853e843cf3cf60a0b3b56ebcbfefb49de
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-01-04 13:26:32 +0000

    + add colorscheme plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,9 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
+  use "navarasu/onedark.nvim"
+  use "shaunsingh/nord.nvim"
+  use {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
