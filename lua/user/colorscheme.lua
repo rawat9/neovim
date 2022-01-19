486ef1217e90d338cf9a39c50748ff9f378873c9
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-01-19 22:02:12 +0000

    + colorscheme change

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,11 @@
+vim.cmd [[ let g:onedark_config = {
+    \ 'style': 'darker',
+\}
+]]
+
 vim.cmd [[
 try
-  colorscheme darkplus
+  colorscheme onedark
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
