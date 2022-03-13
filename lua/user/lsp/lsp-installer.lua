9095ffc1d2b83d1b6594997926bc1fcc3f3acf13
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-03-13 23:44:12 +0000

    TODO: java lsp-config

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -8,7 +8,7 @@ end
 lsp_installer.on_server_ready(function(server)
 	local opts = {
 		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
+		capabilities = require("user.lsp.handlers").capabilitie,
 	}
 
      if server.name == "jsonls" then
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -5,6 +5,7 @@ end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
@@ -14,6 +15,6 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+        -- diagnostics.flake8
 	},
 })
