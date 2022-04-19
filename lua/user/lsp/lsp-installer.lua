7e11e0bf67e2362d335869a66ed3a9f871bfb5a6
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-04-19 23:59:44 +0100

    install prettier[formatting], eslint[diagnostics]

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -8,7 +8,7 @@ end
 lsp_installer.on_server_ready(function(server)
 	local opts = {
 		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilitie,
+		capabilities = require("user.lsp.handlers").capabilities,
 	}
 
      if server.name == "jsonls" then
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,6 +8,7 @@ local formatting = null_ls.builtins.formatting
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
@@ -15,6 +16,7 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-        -- diagnostics.flake8
+        diagnostics.eslint,
+        code_actions.eslint,
 	},
 })
