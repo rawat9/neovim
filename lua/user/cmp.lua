87c0baa6d41a589ce300a4d50857e90c28fd28a3
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-04-20 23:48:34 +0100

    format files using stylua

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -8,13 +8,9 @@ if not snip_status_ok then
     return
 end
 
-<<<<<<< Updated upstream
-require("luasnip/loaders/from_vscode").lazy_load()
-=======
 -- Custom Snippets
 vim.o.runtimepath = vim.o.runtimepath .. ',/Users/anuragrawat/.config/nvim/lua/user/friendly-snippets,'
 require("luasnip/loaders/from_vscode").load()
->>>>>>> Stashed changes
 
 local check_backspace = function()
     local col = vim.fn.col "." - 1
@@ -135,74 +131,5 @@ cmp.setup {
     experimental = {
         ghost_text = false,
         native_menu = false,
-    },
-<<<<<<< Updated upstream
-    -- Accept currently selected item. If none selected, `select` first item.
-    -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-  },
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-      vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[NVIM_LUA]",
-        luasnip = "[Snippet]",
-        buffer = "[Buffer]",
-        path = "[Path]",
-      })[entry.source.name]
-      return vim_item
-    end,
-  },
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "nvim_lua" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-  },
-  confirm_opts = {
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = false,
-  },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
-  experimental = {
-    ghost_text = false,
-    native_menu = false,
-  },
-=======
->>>>>>> Stashed changes
+    }
 }
--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,11 +1,10 @@
 return {
-	settings = {
-
+    settings = {
         python = {
             pythonPath = "/Library/Frameworks/Python.framework/Versions/3.10/bin/python3.10",
             analysis = {
                 typeCheckingMode = "off"
             }
         }
-	},
+    },
 }
