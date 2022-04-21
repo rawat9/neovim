e386cbf576b1e607eb45a789a139ed4acc2510d2
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-04-21 23:51:46 +0100

    update jdtls.lua

--- lua/user/lsp/settings/jdtls.lua
+++ lua/user/lsp/settings/jdtls.lua
@@ -3,33 +3,7 @@ local root_dir = require('jdtls.setup').find_root(root_markers)
 local home = os.getenv('HOME')
 local workspace_folder = home .. "/.workspace" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
-return {
-    settings = {
-        signatureHelp = { enabled = true },
-        contentProvider = { preferred = 'fernflower' },
-        completion = {
-            favoriteStaticMembers = {
-                "org.hamcrest.MatcherAssert.assertThat",
-                "org.hamcrest.Matchers.*",
-                "org.hamcrest.CoreMatchers.*",
-                "org.junit.jupiter.api.Assertions.*",
-                "java.util.Objects.requireNonNull",
-                "java.util.Objects.requireNonNullElse",
-                "org.mockito.Mockito.*"
-            }
-        },
-        sources = {
-            organizeImports = {
-                starThreshold = 9999;
-                staticStarThreshold = 9999;
-            }
-        },
-        codeGeneration = {
-            toString = {
-                template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
-            }
-        },
-    },
+local config = {
     cmd = {
         '/usr/bin/java',
         '-Declipse.application=org.eclipse.jdt.ls.core.id1',
@@ -45,5 +19,36 @@ return {
         '-jar', '~/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
         '-configuration', '~/.local/share/nvim/lsp_servers/jdtls/config_mac',
         '-data', workspace_folder,
-    }
+    },
+
+    settings = {
+        java = {
+            signatureHelp = { enabled = true },
+            contentProvider = { preferred = 'fernflower' },
+            completion = {
+                favoriteStaticMembers = {
+                    "org.hamcrest.MatcherAssert.assertThat",
+                    "org.hamcrest.Matchers.*",
+                    "org.hamcrest.CoreMatchers.*",
+                    "org.junit.jupiter.api.Assertions.*",
+                    "java.util.Objects.requireNonNull",
+                    "java.util.Objects.requireNonNullElse",
+                    "org.mockito.Mockito.*"
+                }
+            },
+            sources = {
+                organizeImports = {
+                    starThreshold = 9999;
+                    staticStarThreshold = 9999;
+                }
+            },
+            codeGeneration = {
+                toString = {
+                    template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+                }
+            },
+        }
+    },
 }
+
+return config
